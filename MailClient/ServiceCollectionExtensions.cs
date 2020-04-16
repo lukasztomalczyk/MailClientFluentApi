8ebcf114961523eb843ab0c668442d3aa73b1ecd
lukasz <lukasz.tomalczyk@wp.pl>
2020-04-16 16:01:08 +0200

    Ref

--- MailClient/ServiceCollectionExtensions.cs
+++ MailClient/ServiceCollectionExtensions.cs
@@ -8,9 +8,9 @@ namespace MailClientFluentApi
         public static void RegisterMailClient(this IServiceCollection service, IConfiguration configuration)
         {
             service.AddScoped<IMailClient>(x => new MailClient(options => {
-                options.SetPort(configuration.GetValue<string>("SMTPClientServer"));
-                options.SetServer(configuration.GetValue<string>("SMTPClientPort"));
-                options.SetSender(configuration.GetValue<string>("SMTPClientSender"));
+                options.SetPort(configuration.GetValue<string>("MailClient:SMTPClientServer"));
+                options.SetServer(configuration.GetValue<string>("MailClient:SMTPClientPort"));
+                options.SetSender(configuration.GetValue<string>("MailClient:SMTPClientSender"));
             }));
         }
     }
